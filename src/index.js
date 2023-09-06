17ce2bde573fc8201721425e00f9c4da84135126
JohnDCGSantos <jdcg.santos@gmail.com>
2023-09-06 10:53:09 +0100

    lab complete second time with extra restart btn

--- src/index.js
+++ src/index.js
@@ -22,82 +22,80 @@ const cards = [
   { name: 'spiderman', img: 'spiderman.jpg' },
   { name: 'superman', img: 'superman.jpg' },
   { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' }
-];
+  { name: 'thor', img: 'thor.jpg' },
+]
 
-const memoryGame = new MemoryGame(cards);
+const memoryGame = new MemoryGame(cards)
+const restartBtn = document.createElement('button')
+restartBtn.textContent = 'Restart Game'
+document.querySelector('#memory-board').appendChild(restartBtn)
 
-function toggle(element, classes) {
-  classes.forEach(className => element.classList.toggle(className));
-}
+window.addEventListener('load', event => {
+  let html = ''
 
-window.addEventListener('load', (event) => {
-  let html = '';
-  memoryGame.cards.forEach((pic) => {
+  memoryGame.cards.forEach(pic => {
     html += `
       <div class="card" data-card-name="${pic.name}">
         <div class="back" name="${pic.img}"></div>
         <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
       </div>
-    `;
-  });
+   `
+  })
 
   // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
-
-  function toggle(element, classes) {
-    classes.forEach(className => element.classList.toggle(className));
-  }
-
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-     const clicked = document.getElementById("pairs_clicked");
-     const guessed = document.getElementById("pairs_guessed");
-
-     console.log("Card clicked: ", card);
-     toggle(card.children[0], ["back", "front"]);
-     toggle(card.children[1], ["back", "front"]);
-
-     memoryGame.pickedCards.push(card);
-     // console.log(memoryGame.pickedCards);
-     if (memoryGame.pickedCards.length === 2) {
-       const firstInPair = memoryGame.pickedCards[0];
-       const secondInPair = memoryGame.pickedCards[1];
-       const cardName1 = firstInPair.getAttribute("data-card-name");
-       const cardName2 = secondInPair.getAttribute("data-card-name");
-       // console.log(cardName1, cardName2);
-
-       if (memoryGame.checkIfPair(cardName1, cardName2)) {
-         firstInPair.children[1].classList.add("blocked");
-         secondInPair.children[1].classList.add("blocked");
-         memoryGame.pickedCards = [];
-
-        if (memoryGame.checkIfFinished()) {
-          document.querySelector("#memory-board").innerHTML = "";
-          let h1 = document.createElement("h1");
-          h1.style.color = "pink";
-          h1.innerHTML = "YOU WON!!!";
-          document.querySelector("#memory-board").appendChild(h1);
+  document.querySelector('#memory-board').innerHTML = html
+  function attachCardClickListeners() {
+    // Bind the click event of each element to a function
+    document.querySelectorAll('.card').forEach(card => {
+      card.addEventListener('click', () => {
+        console.log(`Card clicked: ${card}`)
+        if (memoryGame.pickedCards.length < 2) {
+          memoryGame.pickedCards.push(card)
+          card.classList.add('turned')
+          //memoryGame.pairsClicked++
         }
+        setTimeout(() => {
+          if (memoryGame.pickedCards.length === 2) {
+            const isPair = memoryGame.checkIfPair(
+              memoryGame.pickedCards[0].getAttribute('data-card-name'),
+              memoryGame.pickedCards[1].getAttribute('data-card-name')
+            )
+            document.querySelector('#pairs-clicked').innerHTML = memoryGame.pairsClicked
+            if (isPair) {
+              memoryGame.pickedCards.shift()
+              memoryGame.pickedCards.shift()
+              //memoryGame.pairsGuessed++ alredy in pairsClicked on memory.js...if i use this line  will increment 2*
+              document.querySelector('#pairs-guessed').innerHTML = memoryGame.pairsGuessed
+            } else {
+              memoryGame.pickedCards.forEach(card => {
+                card.setAttribute('class', 'card')
+                memoryGame.pickedCards = []
+              })
+            }
+          }
+          const didYouWin = memoryGame.checkIfFinished()
+          console.log('did you win ', didYouWin)
+          if (didYouWin) {
+            document.querySelector('#memory-board').innerHTML = ''
+            let h1 = document.createElement('h1')
 
-       } else {
-         setTimeout(() => {
-           toggle(firstInPair.children[0], ["back", "front"]);
-           toggle(firstInPair.children[1], ["back", "front"]);
-           toggle(secondInPair.children[0], ["back", "front"]);
-           toggle(secondInPair.children[1], ["back", "front"]);
-         }, 1000);
-         memoryGame.pickedCards = [];
-       }
-       clicked.innerHTML = memoryGame.pairsClicked;
-       guessed.innerHTML = memoryGame.pairsGuessed;
-     }
-
-    });
-  });
-  
-});
-  
-  
+            h1.style.color = 'pink'
+            h1.innerHTML = 'YOU WON!!!'
+            document.querySelector('#memory-board').appendChild(h1)
+            document.querySelector('#memory-board').appendChild(restartBtn)
+          }
+        }, 1500)
+      })
+    })
+  }
+  function resetGame() {
+    memoryGame.reset()
+    document.querySelector('#pairs-clicked').innerHTML = '0'
+    document.querySelector('#pairs-guessed').innerHTML = '0'
+    document.querySelector('#memory-board').appendChild(restartBtn)
+    attachCardClickListeners()
+  }
+  restartBtn.addEventListener('click', resetGame)
+  resetGame()
+  document.querySelector('#memory-board').appendChild(restartBtn)
+})
--- src/memory.js
+++ src/memory.js
@@ -1,45 +1,60 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
-    this.pickedCards = [];
-    this.pairsClicked = 0;
-    this.pairsGuessed = 0;
-    // add the rest of the class properties here
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+    this.shuffleCards()
   }
 
   shuffleCards() {
-    if(!this.cards) {
-      return undefined;
+    if (!this.cards) {
+      return undefined
     }
     for (let i = this.cards.length - 1; i > 0; i--) {
-      const randomIndex = Math.floor(Math.random() * (i + 1));
-      [this.cards[i], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[i]];
+      const randomIndex = Math.floor(Math.random() * (i + 1))
+      ;[this.cards[i], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[i]]
     }
   }
-    // ... write your code here
-  
 
   checkIfPair(card1, card2) {
-    this.pairsClicked++;
+    this.pairsClicked++
 
-    if (card1 === card2){
-      this.pairsGuessed++;
-      return true;
-    }  else {
-        return false;
-      }
-      }
-    
-    // ... write your code here
-  
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
+  }
 
   checkIfFinished() {
-    return this.pairsGuessed === this.cards.length / 2;
-  };
+    if (this.pairsGuessed === this.cards.length / 2) {
+      console.log(this.pairsGuessed, this.cards.length / 2)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  reset() {
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+    this.shuffleCards()
+    this.render()
+  }
+
+  render() {
+    let html = ''
+    this.cards.forEach(pic => {
+      html += `
+        <div class="card" data-card-name="${pic.name}">
+          <div class="back" name="${pic.img}"></div>
+          <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+        </div>
+      `
+    })
+    document.querySelector('#memory-board').innerHTML = html
+  }
 }
-
-
-
-    // ... write your code here}
-
-
